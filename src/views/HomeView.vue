6c636260dab14461a744e9345b8913e67b2e9681
Nito Buendia <16522489+nitobuendia@users.noreply.github.com>
2024-07-12 08:07:27 +0800

    feat: add main home view and vue app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TerritoryStats from '../components/TerritoryStats.vue'
+import TerritoryList from '../components/TerritoryList.vue'
+import TerritoryMap from '../components/TerritoryMap.vue'
+</script>
+
+<template>
+  <TerritoryMap />
+  <TerritoryStats />
+  <TerritoryList />
+</template>
