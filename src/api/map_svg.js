686abd79bbb173f1a4fb4fee1f5a16d229b38274
Nito Buendia <16522489+nitobuendia@users.noreply.github.com>
2024-07-16 07:37:15 +0800

    chore: clean up drawing features into a function

--- src/api/map_svg.js
+++ src/api/map_svg.js
@@ -2,23 +2,31 @@
 import * as d3_geo from 'd3-geo';
 import { default as map_generic } from './map_generic';
 
+const drawFeatures = (svg, path, dataMap, attributes) => {
+  const pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  pathElement.setAttribute('d', path(dataMap));
+  for (const [attributeName, attributeValue] of Object.entries(attributes)) {
+    pathElement.setAttribute(attributeName, attributeValue);
+  }
+  svg.appendChild(pathElement);
+};
+
 const drawMapOnSvg = (svg, dataMap, territoryMap, visitedTerritories) => {
   const outline = { type: 'Sphere' };
 
   const width = parseInt(svg.getAttribute('width'));
   const projection = d3_geo.geoNaturalEarth1().fitWidth(width, outline);
   const height = map_generic.calculateCanvasHeight(projection, outline);
-  // svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
+  svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
   svg.setAttribute('width', width);
   svg.setAttribute('height', height);
 
   const path = d3_geo.geoPath().projection(projection);
 
-  const outlinePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  outlinePath.setAttribute('d', path(outline));
-  outlinePath.setAttribute('stroke', '#ffffff');
-  outlinePath.setAttribute('fill', 'none');
-  svg.appendChild(outlinePath);
+  drawFeatures(svg, path, outline, {
+    'stroke': '#ffffff',
+    'fill': 'none',
+  });
 };
 
 export default {
