ef56758d07796ad9bf1956443c9d54e32969c5a0
Nito Buendia <git@nitobuendia.com>
2025-07-23 20:59:20 +0800

    style: add simple header with logo

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <img src="/logo_64.png" alt="Travel Track Logo" />
+    <h1>Travel Track</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  align-items: center;
+  align-self: center;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+
+  img {
+    height: 64px;
+    margin-right: 24px;
+    width: 64px;
+  }
+
+  h1 {
+    color: #539d6d;
+    font-weight: 600;
+    font-size: 2.6rem;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
+  import Header from '../components/Header.vue';
   import TerritoryStats from '../components/TerritoryStats.vue';
   import TerritoryList from '../components/TerritoryList.vue';
   import TerritoryMap from '../components/TerritoryMap.vue';
 </script>
 
 <template>
+  <Header />
   <TerritoryMap />
   <TerritoryStats />
   <TerritoryList />
